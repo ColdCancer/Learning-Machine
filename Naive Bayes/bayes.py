ce0524ba8d6f2d49522fc434283278a3d4e9f011
ColdCancer <svipvm@yeah.net>
2020-07-01 20:26:37 +0800

    modify bayes.md

--- Naive Bayes/bayes.py
+++ Naive Bayes/bayes.py
@@ -30,3 +30,25 @@ def setOfWordVec(vocabList, inputSet):
             print('the word: %s is not in my vocabulary!')
     return returnVec
 
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # p0Num = zeros(numWords)
+    # p1Num = zeros(numWords)
+    # p0Denom = p1Denom = 0.0
+    p0Num = ones(numWords)
+    p1Num = ones(numWords)
+    p0Denom = p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+    # p0Vect = p0Num / p0Denom
+    # p1Vect = p1Num / p1Denom
+    p0Vect = log(p0Num / p0Denom)
+    p1Vect = log(p1Num / p1Denom)
+    return p0Vect, p1Vect, pAbusive
