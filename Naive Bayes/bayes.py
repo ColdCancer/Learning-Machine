6e76dcb8c17909e6fbd183ef2810d039917084b0
ColdCancer <svipvm@yeah.net>
2020-07-01 11:14:53 +0800

    add bayes.py

---
+++ Naive Bayes/bayes.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time  : 2020/7/1 10:49
+# @Author : VMice
+# ==========================
+
+from numpt import *
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                   ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                   ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWordVec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in inputSet:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print('the word: %s is not in my vocabulary!')
+    return returnVec
+
