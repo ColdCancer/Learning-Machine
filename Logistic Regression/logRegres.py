88e20e95ac22914877f9f3f9affe016eef17b9e7
ColdCancer <svipvm@yeah.net>
2020-07-03 19:57:52 +0800

    modify logRegres.py

--- Logistic Regression/logRegres.py
+++ Logistic Regression/logRegres.py
@@ -27,7 +27,56 @@ def gradAscent(dataMatIn, classLabels):
     weights = ones((n, 1))
     for k in range(maxCycles):
         h = sigmoid(dataMatrix * weights)
-        delta = (labelMat - h)
-        weights += alpha * dataMatrix.transpose() * delta
+        delta = (h - labelMat)
+        weights -= alpha * dataMatrix.transpose() * delta
     return weights
 
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+def stocGradAscent(dataMatrix, classLabels):
+    m, n = shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i] * weights))
+        error = h - classLabels[i]
+        weights -= alpha * error * dataMatrix[i]
+    return weights
+
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.0001
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = h - classLabels[randIndex]
+            weights -= alpha * error * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
